c6df5adb05a720c2bb8e1d0d35a0988132e2333d
Paji <paji_starone@yahoo.co.id>
2018-03-10 12:06:50 +0700

    mekah_update_donatur_laporanpenerimaan

--- mekah.js
+++ mekah.js
@@ -13,6 +13,50 @@ var listPenerima =["Dimas Adi Prasetyo",
                     "Adiknya Fahrul, Cucu Pak Jasmin"
                 ];
 
+var listDonatur =[
+    "Ferry Ferdiansyah"
+	, "Lusy Edi Tasman"
+	, "Ibu Herri, Hj."
+	, "Dairah Buchori, Hj."
+	, "Hetty, Hj."
+	, "Ahmad Kosasih"
+	, "Ibu Ida"
+	, "Hary Poliriyanto"
+	, "Hj. Djuhaeni"
+	, "Arya Budiman"
+	, "Hj. Herawati"
+	, "Hj. Lilia Hanum"
+	, "Faris"
+	, "Hj. Nurliana"
+	, "Ibu Mufidah"
+	, "Marina"
+	, "Hj. Darmawati (Ningsih)"
+	, "Drian S."
+	, "Danik"
+	, "Hj. Rohayani"
+	, "M. Naufal Maulana"
+	, "Almira Balqis"
+	, "Langit Barosa"
+	, "Retno Widowati"
+	, "Hj. Olga Fauziah"
+	, "M. Thariqul"
+	, "Aisyah Suganda"
+	, "Maryah"
+	, "Aenny Solahuddin"
+	, "Hj. Zainab"
+	, "Hj. Siti Hindun"
+	, "Tami"
+	, "Bu Euis"
+	, "Zaidir"
+    , "Hamba Allah 1"];
+    
+    var listPenerimaan =[
+                        ["04-10-2017", "Hetty T", 100000],
+                        ["05-10-2017", "Arya Budiman", 100000],
+                        ["10-10-2017", "Hj. Djahaeni", 150000],
+                        ["10-10-2017", "Aisyah Suganda", 100000],
+];
+
 // ---------------------------------------------------------------------
 $(document).ready(function(){
     var mekahBerjalanKurangLebih = $('#mekahBerjalanKurangLebih');
@@ -20,7 +64,7 @@ $(document).ready(function(){
     var mekahSampaiDenganBulan = $('#mekahSampaiDenganBulan');
     var santunanMekah = $('#santunanMekah');
     var totalPPMekah = $('#totalPPMekah');
-    var tbl = $('#tblPPMekah');
+    
 
     mekahBerjalanKurangLebih.text("4 Bulan");
     mekahBerjalanDariBulan.text("Oktober 2017");
@@ -28,12 +72,32 @@ $(document).ready(function(){
     santunanMekah.text("Rp.100.000");
     totalPPMekah.text(listPenerima.length);
 
+    // Insert Data Penerima Program Mekah
+    var tbl = $('#tblPPMekah');
     // populating column title
     $(tbl).find('thead').append(("<tr> <th> {0} </th> <th> {1} </th> </tr>").format(["No", "Nama"]));
-    
     // populating list
     $.each(listPenerima, function(idx, val){
         $(tbl).find('tbody').append(("<tr> <td> {0} </td> <td> {1} </td> </tr>").format([(idx+1), val]));
     });
     
+
+    // Insert Data Daftar Donatur
+    var tbl = $('#tblDaftarDonatur');
+    // populating column title
+    $(tbl).find('thead').append(("<tr> <th> {0} </th> <th> {1} </th> </tr>").format(["No", "Nama"]));
+    // populating list
+    $.each(listDonatur, function(idx, val){
+        $(tbl).find('tbody').append(("<tr> <td> {0} </td> <td> {1} </td> </tr>").format([(idx+1), val]));
+    });
+
+
+    // Insert Data Laporan Penerimaan
+    var tbl = $('#tblLaporanPenerimaan');
+    // populating column title
+    $(tbl).find('thead').append(("<tr> <th> {0} </th> <th> {1} </th> <th> {2} </th> </tr>").format(["Tanggal", "Nama Donatur", "Nominal"]));
+    // populating list
+    $.each(listPenerimaan, function(idx, val){
+        $(tbl).find('tbody').append(("<tr> <td> {0} </td> <td> {1} </td> <td> {2} </td> </tr>").format([val[0], val[1], val[2]]));
+    });
 });
