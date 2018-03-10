4d21a4f7932881690d87581dc6797ca0d3f41475
Paji <paji_starone@yahoo.co.id>
2018-03-10 15:13:33 +0700

    mekah_update_laporanPenerimaan

--- commons.js
+++ commons.js
@@ -1,3 +1,39 @@
+// 2018-03-10 convert to rupiah, vice versa : https://gist.github.com/faisalman/845309
+/**
+ * JavaScript Code Snippet
+ * Convert Number to Rupiah & vice versa
+ * https://gist.github.com/845309
+ *
+ * Copyright 2011-2012, Faisalman
+ * Licensed under The MIT License
+ * http://www.opensource.org/licenses/mit-license  
+ *
+ */
+ 
+function convertToRupiah(angka)
+{
+	var rupiah = '';		
+	var angkarev = angka.toString().split('').reverse().join('');
+	for(var i = 0; i < angkarev.length; i++) if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
+	return 'Rp. '+rupiah.split('',rupiah.length-1).reverse().join('');
+}
+/**
+ * Usage example:
+ * alert(convertToRupiah(10000000)); -> "Rp. 10.000.000"
+ */
+ 
+function convertToAngka(rupiah)
+{
+	return parseInt(rupiah.replace(/,.*|[^0-9]/g, ''), 10);
+}
+/**
+ * Usage example:
+ * alert(convertToAngka("Rp 10.000.123")); -> 10000123
+ */
+
+
+
+
 // 2018-02-23 string format in javascript : https://www.codeproject.com/Tips/201899/String-Format-in-JavaScript
 
 String.prototype.format = function (args) {
@@ -18,6 +54,8 @@ String.prototype.format = function (args) {
     });
 };
 String.prototype.format.regex = new RegExp("{-?[0-9]+}", "g");
+
+
 
 
 // 2018-02-23 Go to spesific tab in boostrab : https://stackoverflow.com/questions/7862233/twitter-bootstrap-tabs-go-to-specific-tab-on-page-reload-or-hyperlink
--- mekah.js
+++ mekah.js
@@ -51,11 +51,73 @@ var listDonatur =[
     , "Hamba Allah 1"];
     
     var listPenerimaan =[
-                        ["04-10-2017", "Hetty T", 100000],
-                        ["05-10-2017", "Arya Budiman", 100000],
-                        ["10-10-2017", "Hj. Djahaeni", 150000],
-                        ["10-10-2017", "Aisyah Suganda", 100000],
+                        ["10/04/2017", "Hetty T", 100000],
+                        ["10/05/2017", "Arya Budiman", 100000],
+                        ["10/10/2017", "Hj. Djahaeni", 150000],
+                        ["10/10/2017", "Aisyah Suganda", 100000],
+						["10/15/2017", "Ibu Ida", 50000],
+						["10/15/2017", "Almira Balqis", 20000],
+						["10/15/2017", "Retno Widowati", 20000],
+						["10/15/2017", "Langit Barosa",	 20000],
+						["10/17/2017", "Tami",	 500000], 
+						["10/17/2017", "Bu Dairah",	 200000],
+						["10/1/2017",	"Bu Hindun",	 100000],
+						["10/1/2017",	"Bu Diah",	 100,000], 
+						["10/15/2017",	"Celengan Bu Dairah",	 180300],
+						["11/1/2017",	"Faris",	 100000], 
+						["11/1/2017",	"Maryah",	 30000], 
+						["11/1/2017",	"Mufidah",	 20000], 
+						["11/1/2017",	"M. Thariqul",	 100000],
+						["11/9/2017",	"Danir",	 60000],
+						["11/9/2017",	"M. Naufal Maulana",	 75000], 
+						["11/9/2017",	"Hj. Hetty",	 100000],
+						["11/9/2017",	"Hj. Olga Fauziah",	 40000],
+						["11/12/2017",	"Aenny Salahuddin",	 600000], 
+						["11/12/2017",	"Dairah Buchori",	 100000], 
+						["11/12/2017",	"Hj. Nurliana S.",	 150000], 
+						["11/19/2017",	"Hamba Allah 1",	 500000], 
+						["11/21/2017",	"Aisyah Suganda",	 100000], 
+						["11/20/2017",	"Ibu Ida",	 50000], 
+						["11/28/2017",	"Hj. Herawati",	 100000], 
+						["12/4/2017",	"Hj. Rohayani",	 15000], 
+						["12/4/2017",	"Tami",	 500000], 
+						["12/4/2017",	"Arya Budiman",	 100000], 
+						["12/5/2017",	"Hj. Hetty",	 100000], 
+						["12/12/2017",	"Hj. Olga Fauziah",	 20000], 
+						["12/12/2017",	"Hj. Aisiah",	 100000], 
+						["12/15/2017",	"Ibu Ida",	 100000], 
+						["1/3/2018",	"Hj. Dairah B",	 100000], 
+						["1/3/2018",	"Hj. Djuhaeni",	 100000], 
+						["1/8/2018",	"Arya Budiman",	 100000], 
+						["1/9/2018",	"Hj. Zainab", 50000], 
+						["1/9/2018",	"Drian S.",	 125000], 
+						["1/9/2018",	"Langit Barosa",	 20000], 
+						["1/9/2018",	"Almira Balqis",	 20000], 
+						["1/9/2018",	"Retno Widowati",	 20000], 
+						["1/9/2018",	"Hj. Darmawanti",	 200000], 
+						["1/9/2018",	"Hj. Hetty",	 100000], 
+						["1/21/2018",	"Hj. Nurliana S.",	 150000], 
+						["1/23/2018",	"Ibu Mufidah",	 20000], 
+						["1/23/2018",	"Faris",	 100000], 
+						["1/29/2018",	"Hj. Rohayani",	 45000], 
+						["1/30/2018",	"Marina",	 60000], 
+						["1/31/2018",	"Arya Budiman",	 50000], 
+						["2/6/2018",	"M. Naufal Maulana",	 50000], 
+						["2/6/2018",	"Hj. Hetty",	 100000], 
+						["2/4/2018",	"Hj. Siti Hindun",	 100000], 
+						["2/14/2018",	"Drian S.",	 50000], 
+						["2/14/2018",	"Faris",	 50000], 
+						["2/14/2018",	"Hj. Olga Fauziah",	 20000], 
+						["2/14/2018",	"Ibu Mufidah", 	 40000], 
+						["2/18/2018",	"Ida Tarmizi", 	 50000], 
+						["2/18/2018",	"Tami", 	 1000000], 
+						["2/18/2018",	"Bp. Zaidir",	 270000], 
+						["2/20/2018",	"Bu Euis",	 100000] 
+
+                        
 ];
+
+
 
 // ---------------------------------------------------------------------
 $(document).ready(function(){
@@ -95,9 +157,9 @@ $(document).ready(function(){
     // Insert Data Laporan Penerimaan
     var tbl = $('#tblLaporanPenerimaan');
     // populating column title
-    $(tbl).find('thead').append(("<tr> <th> {0} </th> <th> {1} </th> <th> {2} </th> </tr>").format(["Tanggal", "Nama Donatur", "Nominal"]));
+    $(tbl).find('thead').append(("<tr> <th> {0} </th> <th> {1} </th> <th> {2} </th> <th> {3} </th> </tr>").format(["No", "Tanggal", "Nama Donatur", "Nominal"]));
     // populating list
     $.each(listPenerimaan, function(idx, val){
-        $(tbl).find('tbody').append(("<tr> <td> {0} </td> <td> {1} </td> <td> {2} </td> </tr>").format([val[0], val[1], val[2]]));
+        $(tbl).find('tbody').append(("<tr> <td> {0} </td> <td> {1} </td> <td> {2} </td> <td> {3} </td> </tr>").format([(idx+1), val[0], val[1], convertToRupiah(val[2])]));
     });
 });
